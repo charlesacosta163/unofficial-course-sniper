de4b95b87ec98af3c1b94266848bcf3f29138bbe
charlesacosta163 <charlesacosta163@gmail.com>
2024-02-25 21:38:03 -0500

    Added courses api fetching from express

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -10,19 +10,27 @@ const Form = () => {
     const { user, setUser } = useContext(UserContext);
 
     const [selectedTerm, setSelectedTerm] = useState('fall');
-    const [selectedDept, setSelectedDept] = useState('');
     const [filteredCourseData, setFilteredCourseData] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        // For local json course data access, use '../src/data.json'
-        // fetch(`http://localhost:5000/api/courses/search?title=${searchTerm}`)
-        fetch(`http://localhost:5000/api/courses/search?title=${searchTerm}`)
-            .then(res => res.json())
-            .then(data => {
+        const fetchData = async () => {
+            if (searchTerm.length < 3) {
+                return;
+            }
+    
+            setLoading(true);
+            
+            // Fetch the courses via substring search of the search term
+            try {
+                const response = await fetch(`http://localhost:5000/api/courses/search?title=${searchTerm}`);
+                const data = await response.json();
+    
                 const filteredTerm = data.filter(course => course.term.toLowerCase().includes(selectedTerm.toLowerCase()));
-                const filteredSection = filteredTerm.filter(course => course.title.toLowerCase().includes(selectedDept.toLowerCase()));
-                const filteredBySearch = filteredSection.filter(course => course.title.toLowerCase().includes(searchTerm.toLowerCase()));
+                const filteredBySearch = filteredTerm.filter(course => course.title.toLowerCase().includes(searchTerm.toLowerCase()));
+
+                // Course Objects Mapping
                 const mappedFilteredCourseData = filteredBySearch.map((course, index) => (
                     <Class
                         key={index}
@@ -37,14 +45,22 @@ const Form = () => {
                     />
                 ));
                 setFilteredCourseData(mappedFilteredCourseData);
-            })
-            .catch(error => {
+                setLoading(false); // Set loading state to false when data fetching is done
+    
+            } catch (error) {
                 console.error('Error fetching courses:', error);
-            });
-    }, [selectedTerm, selectedDept, searchTerm]);
+                setLoading(false); // Set loading state to false if there's an error
+            }
+        };
+    
+        fetchData(); // Call the async function immediately
+    }, [selectedTerm, searchTerm]);
 
-    const handleAddEntry = (sectionName, professor, id, title, startDate, seats) => {
+    const handleAddEntry = async (sectionName, professor, id, title, startDate, seats) => {
         const [name, section, number] = sectionName.split('-');
+        console.log(user.studentId);
+
+        // Each Course Object as newEntry
         const newEntry = {
             id: id,
             code: name,
@@ -55,48 +71,53 @@ const Form = () => {
             startDate: startDate,
             availableSeats: seats
         };
-        setUser(prevUser => {
-            const updatedTargetCourses = [...prevUser.targetCourses, newEntry];
-            return { ...prevUser, targetCourses: updatedTargetCourses };
+
+        // Attempt an user PUT request on targetCourses when handleAddEntry function is fired
+        try {
+
+            // Get current user targetCourses array and update with the newEntry object
+            const updatedTargetCourses = [...user.targetCourses, newEntry];
+
+            // PUT Request, fetches the express server
+            await fetch(`http://localhost:5000/api/students/${user.studentId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ targetCourses: updatedTargetCourses }) // Only update targetCourses in the DB
         });
+        
+        // Set the current user information with the targetCourses array updated locally
+        setUser(prevUser => ({ ...prevUser, targetCourses: updatedTargetCourses }));
+
+        console.log("Target courses updated successfully");
+
+        } catch (error) {
+            console.log("Error", error);
+        }
+
     }
 
     return (
         <section id="form-section" className={`${sectionWidth} h-auto flex flex-col items-center`}>
 
-            <div id="form-container" className={`max-w-[738px] w-full p-8 z-[.2] rounded ${darkMode ? "bg-green" : "bg-primary"}`}>
+            <div id="form-container" className={`max-w-[738px] w-full p-8 z-[.2] rounded-[20px] ${darkMode ? "bg-green" : "bg-primary"}`}>
 
-                <Link to="/">
-                    <div className="pb-4 sm:pb-2 text-[1.5rem] inline-block text-light"><FaArrowLeft /></div>
-                </Link>
-                <h1 className={`text-center ${headerText} mb-8 text-light`}>Snipe A Class</h1>
+                <div className="flex justify-between items-center pb-4">
+                    <Link to="/">
+                        <div className="text-[1.5rem] inline-block text-light"><FaArrowLeft /></div>
+                    </Link>
+                    <h1 className={`text-center ${headerText} sm:text-[1.5rem] text-light`}>Snipe A Class</h1>
+                </div>
 
-                <div id="forms" className={`flex flex-col gap-4 text-dark`}>
-                    <div className="select flex justify-between">
+                <div className="select flex flex-col gap-4">
 
-                        <div className="text-dark">
-                            <label htmlFor="term" className="block text-xs font-[500] mb-2 text-light">Term</label>
-                            <select name="terms" value={selectedTerm} id="" onChange={e => setSelectedTerm(e.target.value)} className="rounded">
-                                <option value="fall">Fall 2023</option>
-                                <option value="spring">Spring 2024</option>
-                            </select>
-                        </div>
-
-                        <div className={`text-dark`}>
-                            <label htmlFor="term" className="block text-xs font-[500] mb-2 text-light">Department</label>
-                            <select name="terms" value={selectedDept} id="" onChange={e => setSelectedDept(e.target.value)} className="rounded">
-                                <option value="">All</option>
-                                <option value="art">Art</option>
-                                <option value="accounting">Accounting</option>
-                                <option value="biology">Biology</option>
-                                <option value="chemistry">Chemistry</option>
-                                <option value="computer">Computer</option>
-                                <option value="history">History</option>
-                                <option value="math">Mathematics</option>
-                                <option value="business">Business</option>
-                                <option value="criminal">Criminal Justice</option>
-                            </select>
-                        </div>
+                    <div className="text-dark">
+                        <label htmlFor="term" className="block text-xs font-[500] mb-2 text-light">Term</label>
+                        <select name="terms" value={selectedTerm} id="" onChange={e => setSelectedTerm(e.target.value)} className="rounded">
+                            <option value="fall">Fall 2023</option>
+                            <option value="spring">Spring 2024</option>
+                        </select>
                     </div>
 
                     <div className="w-full">
@@ -107,8 +128,14 @@ const Form = () => {
 
             </div>
 
-            <div id="classes-container" className={`grid grid-cols-2 md:grid-cols-1 gap-4 max-w-[800px] w-full p-8 mt-4 sm:p-2 z-[.2] rounded ${darkMode ? "bg-[#F8F8FF]" : " text-fontDarkMode"}`}>
-                {filteredCourseData.length === 0 ? <span className="text-center">No classes found, try again.</span> : filteredCourseData}
+            <div id="classes-container" className={`grid ${loading ? 'grid-cols-1' : 'grid-cols-2'} md:grid-cols-1 gap-4 max-w-[800px] w-full p-8 mt-4 sm:p-2 z-[.2] rounded ${darkMode ? "bg-[#F8F8FF]" : " text-fontDarkMode"}`}>
+                {loading ? ( // Render loading spinner if loading is true
+                    <div className="w-full flex justify-center items-center">
+                        <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-bgGrayBtn"></div>
+                    </div>
+                ) : (
+                    searchTerm.length < 3 ? <div className="w-full"><span className="text-center">Minimum of 3 characters required</span></div> : filteredCourseData.length < 1 ? "No courses found" : filteredCourseData
+                )}
             </div>
         </section>
     )
